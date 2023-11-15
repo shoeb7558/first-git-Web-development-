a3bbedeb0c1409b85a2fa299198ed123a18df2de
shoeb7558 <83645262+shoeb7558@users.noreply.github.com>
2023-11-15 11:40:43 +0530

    Update_localStorage_1.1
    in this update I am storing the user data in the local storage

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-
 const btn = document.querySelector('.btn');
 btn.addEventListener('click', e => {
     e.preventDefault();
-    console.log(e.target.className);
-    document.getElementById('my-form').style.background = '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
-    
-  });
+    const myForm = document.getElementById('my-form');
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
 
-  btn.addEventListener('mouseover', e => {
-    e.preventDefault();
-    console.log(e.target.className);
-    document.getElementById('my-form').style.background = '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
-    
-  });
+    if (name.trim() === '' || email.trim() === '') {
+        alert('Please enter both name and email');
+        return;
+    }
 
-  
-  const btn1 = document.querySelector('.btn1');
-  btn.addEventListener('mouseout', e => {
-    e.preventDefault();
-    console.log(e.target.className);
+    // Create an object to store user data
+    const userData = {
+        name: name,
+        email: email
+    };
+
+    // Convert the object to a JSON string and store it in local storage
+    localStorage.setItem('userData', JSON.stringify(userData));
+
+    myForm.reset();
+
+    // Change the background and class of the form
     document.getElementById('my-form').style.background = '#ccc';
     document.querySelector('body').classList.add('bg-dark');
-    
-  });
+});
