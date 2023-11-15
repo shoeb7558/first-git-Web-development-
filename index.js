869b74850c2b5bc77f394e70642de32b769876cf
shoeb7558 <83645262+shoeb7558@users.noreply.github.com>
2023-11-15 11:45:48 +0530

    Update_local_storage_1.1

--- index.js
+++ index.js
@@ -2,26 +2,45 @@
 const btn = document.querySelector('.btn');
 btn.addEventListener('click', e => {
     e.preventDefault();
+    const myForm = document.getElementById('my-form');
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    if (name.trim() === '' || email.trim() === '') {
+        alert('Please enter both name and email');
+        return;
+    }
+
+    // Create an object to store user data
+    const userData = {
+        name: name,
+        email: email
+    };
+
+    // Convert the object to a JSON string and store it in local storage
+    localStorage.setItem('userData', JSON.stringify(userData));
+
+    myForm.reset();
     console.log(e.target.className);
     document.getElementById('my-form').style.background = '#ccc';
     document.querySelector('body').classList.add('bg-dark');
     
   });
 
-  btn.addEventListener('mouseover', e => {
-    e.preventDefault();
-    console.log(e.target.className);
-    document.getElementById('my-form').style.background = '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
+  //btn.addEventListener('mouseover', e => {
+    //e.preventDefault();
+    //console.log(e.target.className);
+    //document.getElementById('my-form').style.background = '#ccc';
+    //document.querySelector('body').classList.add('bg-dark');
     
-  });
+  //});
 
   
-  const btn1 = document.querySelector('.btn1');
-  btn.addEventListener('mouseout', e => {
-    e.preventDefault();
-    console.log(e.target.className);
-    document.getElementById('my-form').style.background = '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
+  //const btn1 = document.querySelector('.btn1');
+  //btn.addEventListener('mouseout', e => {
+    //e.preventDefault();
+    //console.log(e.target.className);
+    //document.getElementById('my-form').style.background = '#ccc';
+    //document.querySelector('body').classList.add('bg-dark');
     
-  });
+  //});
